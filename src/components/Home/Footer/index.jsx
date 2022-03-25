cff219b88f7dbc5c2022bb7f523381c274b505b2
Satyam Pariyar <pariyarsatyam1@gmail.com>
2022-03-25 18:38:21 +0530

    added basic routing

--- src/components/Home/Footer/index.jsx
+++ src/components/Home/Footer/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export function Footer() {
     return (
@@ -14,9 +15,9 @@ export function Footer() {
                         <li>© 2022, All Rights Reserved</li>
                     </div>
                     <div className="footer-icons">
-                        <li><a href="https://github.com/pariyar07"><i className="fab fa-github"></i></a></li>
-                        <li><a href="https://twitter.com/satyamP_js"><i className="fab fa-twitter"></i></a></li>
-                        <li><a href="https://hashnode.com/@SatyamP"><i className="fa fa-rss"></i></a></li>
+                        <li><a href="https://github.com/pariyar07" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a></li>
+                        <li><a href="https://twitter.com/satyamP_js" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a></li>
+                        <li><a href="https://hashnode.com/@SatyamP" target="_blank" rel="noreferrer"><i className="fa fa-rss"></i></a></li>
                     </div>
                 </div>
                 <div className="footer middle-left-info">
@@ -28,11 +29,21 @@ export function Footer() {
                 </div>
                 <div className="footer middle-right-info">
                     <h1>Account</h1>
-                    <li><a href="./sign-up.html">My Account</a></li>
-                    <li><a href=".">Wishlist</a></li>
-                    <li><a href="./cart.html">View Cart</a></li>
-                    <li><a href=".">Track Order</a></li>
-                    <li><a href=".">Privacy Policy</a></li>
+                    <li>
+                    <Link to="/signup">My Account</Link>
+                    </li>
+                    <li>
+                    <Link to="/wishlist">Wishlist</Link>
+                    </li>
+                    <li>
+                    <Link to="/cart">View Cart</Link>
+                    </li>
+                    <li>
+                    <a href=".">Track Order</a>
+                    </li>
+                    <li>
+                    <a href=".">Privacy Policy</a>
+                    </li>
                 </div>
                 <div className="footer right-info">
                     <h1>Contact Us</h1>
