7acb1f0402bbd6947ba85ba238310adb7caa08ca
Satyam Pariyar <pariyarsatyam1@gmail.com>
2022-03-26 14:13:36 +0530

    Homepage updates

--- src/fileModules.jsx
+++ src/fileModules.jsx
@@ -1,11 +1,12 @@
-import { DealsCard } from "components/Home/common/deals-card"
 import { Banner } from "components/Home/common/main-banner"
 import { Navbar } from 'components/Home/Navbar/index'
 import { Footer } from 'components/Home/Footer/index'
+import { MainHorizontalCard } from 'components/Home/common/main-horizontal-card'
 import { HorizontalCard } from 'components/Home/common/horizontal-card'
 import { CartNavbar} from "components/Home/common/cart-navbar"
 import {Main} from "components/Home/Main/index"
 import {VerticalCard} from 'components/Home/common/vertical-card'
 import {ProductNavbar} from 'components/Home/common/product-listing-navbar'
+import {Laceless} from 'components/Home/common/laceless'
 
-export {Navbar, Main, Footer, HorizontalCard, CartNavbar, DealsCard, Banner, VerticalCard, ProductNavbar}
+export {Navbar, Main, Footer, HorizontalCard, MainHorizontalCard,  CartNavbar, Banner, VerticalCard, ProductNavbar, Laceless}
